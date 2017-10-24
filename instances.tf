ea9e61b1a7aa8ecf44478590092a7e8481bf5775
Andrey Larin <andrey.larin@russianpost.ru>
2017-10-24 19:47:26 +0300

    adds instances and nat

---
+++ instances.tf
@@ -0,0 +1,23 @@
+resource "aws_instance" "instance1" {  
+  ami           = "ami-1e339e71"
+  instance_type = "t2.nano"
+  vpc_security_group_ids = ["${aws_security_group.sgroup-1.id}", 
+                            "${data.aws_security_group.default-sg.id}"]
+  subnet_id = "${aws_subnet.main-public-1.id}"
+  key_name = "mykeypair"
+
+  tags {
+    Name = "Public instance "    
+  }
+}
+
+resource "aws_instance" "instance2" {  
+  ami           = "ami-1e339e71"
+  instance_type = "t2.nano"  
+  subnet_id = "${aws_subnet.main-private-1.id}"
+  key_name = "mykeypair"
+
+  tags {
+    Name = "Private Instance"
+  }
+}
