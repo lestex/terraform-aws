c81ae71130d5a0478663820b5b415f329223d6ff
lestex <lestex@gmail.com>
2017-11-02 18:28:03 +0300

    adds packer

---
+++ 12 - aws-packer/vars.tf
@@ -0,0 +1,11 @@
+variable "AWS_REGION" {
+  default = "eu-central-1"
+}
+
+variable "PATH_TO_PRIVATE_KEY" {
+  default = "~/.ssh/id_rsa.pub"
+}
+
+variable "PATH_TO_PUBLIC_KEY" {
+  default = "~/.ssh/id_rsa.pub"
+}
