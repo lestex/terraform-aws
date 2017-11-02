2921c7ca04e30852bcdfce27960f6d0a638d4edc
lestex <lestex@gmail.com>
2017-11-02 14:40:19 +0300

    adds changes to S3 and network

--- 05 - aws-ec2-s3/network.tf
+++ 05 - aws-ec2-s3/network.tf
@@ -1,13 +1,33 @@
-# Internet VPC
+# Create VPC
 resource "aws_vpc" "main" {
-    cidr_block = "10.0.0.0/16"
-    instance_tenancy = "default"
-    enable_dns_support = "true"
-    enable_dns_hostnames = "true"
-    enable_classiclink = "false"
-    tags {
-        Name = "main"
-    }
+  cidr_block = "10.0.0.0/16"
+  instance_tenancy = "default"
+  enable_dns_support = "true"
+  enable_dns_hostnames = "true"
+  enable_classiclink = "false"
+  tags {
+    Name = "main"
+  }
+}
+
+# Create Internet GW
+resource "aws_internet_gateway" "main-gw" {
+  vpc_id = "${aws_vpc.main.id}"
+  tags {
+    Name = "main"
+  }
+}
+
+# Create route tables
+resource "aws_route_table" "main-public" {
+  vpc_id = "${aws_vpc.main.id}"
+  route {
+    cidr_block = "0.0.0.0/0"
+    gateway_id = "${aws_internet_gateway.main-gw.id}"
+  }
+  tags {
+    Name = "main-public-1"
+  }
 }
 
 # Subnets
@@ -15,7 +35,7 @@ resource "aws_subnet" "main-public-1" {
     vpc_id = "${aws_vpc.main.id}"
     cidr_block = "10.0.1.0/24"
     map_public_ip_on_launch = "true"
-    availability_zone = "eu-west-1a"
+    availability_zone = "eu-central-1a"
 
     tags {
         Name = "main-public-1"
@@ -25,7 +45,7 @@ resource "aws_subnet" "main-public-2" {
     vpc_id = "${aws_vpc.main.id}"
     cidr_block = "10.0.2.0/24"
     map_public_ip_on_launch = "true"
-    availability_zone = "eu-west-1b"
+    availability_zone = "eu-central-1b"
 
     tags {
         Name = "main-public-2"
@@ -35,17 +55,18 @@ resource "aws_subnet" "main-public-3" {
     vpc_id = "${aws_vpc.main.id}"
     cidr_block = "10.0.3.0/24"
     map_public_ip_on_launch = "true"
-    availability_zone = "eu-west-1c"
+    availability_zone = "eu-central-1c"
 
     tags {
         Name = "main-public-3"
     }
 }
+
 resource "aws_subnet" "main-private-1" {
     vpc_id = "${aws_vpc.main.id}"
     cidr_block = "10.0.4.0/24"
     map_public_ip_on_launch = "false"
-    availability_zone = "eu-west-1a"
+    availability_zone = "eu-central-1a"
 
     tags {
         Name = "main-private-1"
@@ -55,7 +76,7 @@ resource "aws_subnet" "main-private-2" {
     vpc_id = "${aws_vpc.main.id}"
     cidr_block = "10.0.5.0/24"
     map_public_ip_on_launch = "false"
-    availability_zone = "eu-west-1b"
+    availability_zone = "eu-central-1b"
 
     tags {
         Name = "main-private-2"
@@ -65,32 +86,10 @@ resource "aws_subnet" "main-private-3" {
     vpc_id = "${aws_vpc.main.id}"
     cidr_block = "10.0.6.0/24"
     map_public_ip_on_launch = "false"
-    availability_zone = "eu-west-1c"
+    availability_zone = "eu-central-1c"
 
     tags {
         Name = "main-private-3"
-    }
-}
-
-# Internet GW
-resource "aws_internet_gateway" "main-gw" {
-    vpc_id = "${aws_vpc.main.id}"
-
-    tags {
-        Name = "main"
-    }
-}
-
-# route tables
-resource "aws_route_table" "main-public" {
-    vpc_id = "${aws_vpc.main.id}"
-    route {
-        cidr_block = "0.0.0.0/0"
-        gateway_id = "${aws_internet_gateway.main-gw.id}"
-    }
-
-    tags {
-        Name = "main-public-1"
     }
 }
 
--- 05 - aws-ec2-s3/s3.tf
+++ 05 - aws-ec2-s3/s3.tf
@@ -1,8 +1,9 @@
-resource "aws_s3_bucket" "myfirst-bucket" {
-    bucket = "mybucket-c29df1"
+resource "aws_s3_bucket" "b" {
+    bucket = "devopsprobucket-c29df1"
     acl = "private"
 
     tags {
-        Name = "mybucket-c29df1"
+        Name = "devopsprobucket-c29df1",
+        Creator = "Terraform"
     }
 }
