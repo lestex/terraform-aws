ee013531997abcbc1472364b172d8efa29630bdc
Andrey Larin <lestex@gmail.com>
2017-11-04 22:28:43 +0300

    changes to packer example

--- 12 - aws-packer/instance.tf
+++ 12 - aws-packer/instance.tf
@@ -6,7 +6,7 @@ resource "aws_instance" "example" {
   subnet_id = "${aws_subnet.main-public-1.id}"
 
   # the security group
-  vpc_security_group_ids = ["${aws_security_group.example-instance.id}"]
+  vpc_security_group_ids = ["${aws_security_group.instance-sg.id}"]
 
   # the public SSH key
   key_name = "${aws_key_pair.mykeypair.key_name}"
