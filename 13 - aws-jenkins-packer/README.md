31c3b4207943f99f369183ffd267a70920b34789
lestex <lestex@gmail.com>
2017-11-08 15:35:10 +0300

    adds aws-packer-jenkins

---
+++ 13 - aws-jenkins-packer/README.md
@@ -0,0 +1,20 @@
+## This is a basic AWS Terraform project
+
+What it does:
+
+- Adds Packer for image (ami) provisioning
+- Adds Jenkins
+
+### Variables
+
+"AWS_REGION" defaults to "eu-central-1"
+
+### Usage
+
+Initialize:
+
+    ./build-and-run.sh
+
+Destroy:
+
+    terraform destroy
---
+++ 13 - aws-jenkins-packer/vars.tf
@@ -0,0 +1,33 @@
+variable "AWS_REGION" {
+  default = "eu-central-1"
+}
+
+variable "PATH_TO_PRIVATE_KEY" {
+  default = "~/.ssh/id_rsa.pub"
+}
+
+variable "PATH_TO_PUBLIC_KEY" {
+  default = "~/.ssh/id_rsa.pub"
+}
+
+variable "AMIS" {
+  type = "map"
+
+  default = {
+    us-east-1 = "ami-13be557e"
+    us-west-2 = "ami-06b94666"
+    eu-west-1 = "ami-844e0bf7"
+  }
+}
+
+variable "INSTANCE_DEVICE_NAME" {
+  default = "/dev/xvdh"
+}
+
+variable "JENKINS_VERSION" {
+  default = "2.32.1"
+}
+
+variable "APP_INSTANCE_COUNT" {
+  default = "0"
+}
