aff1541d0e892793d0ca90d00076701375f59a64
lestex <lestex@gmail.com>
2017-11-08 13:59:56 +0300

    adds aws-beanstalk

---
+++ 08 - aws-beanstalk/vars.tf
@@ -0,0 +1,13 @@
+variable "AWS_REGION" {
+  default = "eu-west-1"
+}
+
+variable "PATH_TO_PRIVATE_KEY" {
+  default = "~/.ssh/id_rsa"
+}
+
+variable "PATH_TO_PUBLIC_KEY" {
+  default = "~/.ssh/id_rsa.pub"
+}
+
+variable "RDS_PASSWORD" {}
